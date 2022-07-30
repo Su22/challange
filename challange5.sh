3fe1dbee012767b73f59bf637c346fe307c4a06b
sujan <suzan.thapa2221@gmail.com>
2022-07-30 21:53:33 +0000

    adding challange5

--- challange5.sh
+++ challange5.sh
@@ -3,19 +3,16 @@
 # Script: running process               
 # Author:sujan thapa magar                       
 # Date of latest revision:07/30/2022 
-# Purpose:                      
-
-# Basic for loop
-pgrep $1 2>&1 > /dev/null
-if [ $? -eq 0 ]
-then
-{
-    echo " "$1" PROCESS RUNNING "
-    ps -ef | grep $1 | grep -v grep | awk '{print $2}'| xargs kill -9
-}
-else
-{
-    echo "  NO $1 PROCESS RUNNING"
-};fi
-
-# End
+# Purpose: Write a script that displays running processes, asks the user for a PID, then kills the process with that PID.    
+while answer=y
+ps aux        
+# ask user input
+do echo enter PID you want to kill 
+# put the pid in the veriable pid1
+read PID1
+#kill process
+sudo kill -9 $PID1
+echo you enter this number; $PID1
+read answer
+done
+# EndS
