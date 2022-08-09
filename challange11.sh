3841245f855a980dc4be59b5a40c720c3467418f
Su22 <suzan.thapa2221@gmail.com>
2022-08-08 19:12:02 -0700

    Create challange11.sh

---
+++ challange11.sh
@@ -0,0 +1,25 @@
+# Script: Ops 201 Class 11
+# Author: sujan thapa magar
+# Date of latest revision: August 8, 2022
+# Purpose: This script contains a collection of commands that automate Windows  system configurations
+ 
+# Main
+# Enable File and Printer Sharing
+Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Enabled True
+
+# Allow ICMP traffic
+netsh advfirewall firewall add rule name="Allow incoming ping requests IPv4" dir=in action=allow protocol=icmpv4
+
+# Enable remote management 
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+
+# Remove bloatware
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+
+# Enable Hyper-V
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+
+# Disable SMBv1, an insecure protocol
+Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force
+
+# End
